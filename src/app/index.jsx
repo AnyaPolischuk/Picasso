bb6666354be7b7ea61b7c2d4e95da2ac1e258785
a.polischuk <a.polischuk@adriver.ru>
2023-11-09 22:01:59 +0300

    chore: add styles for all project and App component

---
+++ src/app/index.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+import { Routing } from "../pages/index";
+import "./index.scss"
+
+export const  App = () => {
+  return (
+    <>
+      <Routing />
+    </>
+  )  
+}
